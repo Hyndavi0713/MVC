2c134100e4a75d0b2ed6fd5b8fda5e9db6415f0a
Hyndavi0713 <118278386+Hyndavi0713@users.noreply.github.com>
2022-12-04 12:22:53 +0000

    HOL2

---
+++ HomeController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _HOL2.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index(string id)
+        {
+            if (id == null)
+            {
+                return Content("<h1>this is a demo of contentresult</h1>,");
+            }
+            else if (id.ToLower() == "plain")
+            {
+                return Content("<h1> this is a demo of contentresult</h1>", "text/plain");
+            }
+            else if (id.ToLower() == "html")
+            {
+                return Content("<h1> this is a demo of contentResult</h1>", "text/html");
+            }
+            else if (id.ToLower() == "xml")
+            {
+                return Content("<h1> this a demo of contentresult<h1>", "text/xml");
+            }
+            return Content("Invalid content type");
+        }
+        public ActionResult DownloadFile()
+        {
+            if (!System.IO.File.Exists(@"D:\hyndavi.jpg"))
+            {
+                return Content("File to be downloaded not found");
+            }
+            return File(@"D:\hyndavi.jpg", "image/jpg", "default.jpg");
+        }
+        public ActionResult Google()
+        {
+            return Redirect("http://www.google.com");
+        }
+        public ActionResult StartPage()
+        {
+            return RedirectToAction("Index", new { id = "xml" });
+        }
+        public ActionResult Login()
+        {
+            return View();
+        }
+        public ActionResult AboutUs()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ Product.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace _HOL2.Models
+{
+    public class Product
+    {
+        public int productId { get; set; }
+        public string Productname { get; set; }
+        public double Price { get; set; }
+        public IEnumerable<Product> GetProducts()
+        {
+            return new List<Product>()
+            {
+                new Product{productId=1,Productname="Product1",Price=900},
+            new Product { productId = 2, Productname = "Product2", Price = 700 },
+            new Product { productId = 3, Productname = "Product3", Price = 650 },
+            new Product { productId = 4, Productname = "Product4", Price = 950 },
+            new Product { productId = 5, Productname = "Product5", Price = 800 },
+
+        };
+        }
+    }
+}
+
+        
+    
